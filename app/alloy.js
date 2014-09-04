e9ceea067f67fe4e0bd390e48aa3a4328d8381f8
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-09-04 12:06:19 +0000

    * created new widget (com.mscripts.drawermenu) as a replacement for navigator.js (Tested on iOS, Android & MobileWeb) * config.json - all style configurations are removed. Recommend to use Themes.

--- app/alloy.js
+++ app/alloy.js
@@ -1,11 +1,14 @@
-// The contents of this file will be executed before any of
-// your view controllers are ever executed, including the index.
-// You have access to all functionality on the `Alloy` namespace.
-//
-// This is a great place to do any initialization for your app
-// or create any global variables/functions that you'd like to
-// make available throughout your app. You can easily make things
-// accessible globally by attaching them to the `Alloy.Globals`
-// object. For example:
-//
-// Alloy.Globals.someGlobalFunction = function(){};
+var isIOS7Plus = function() {
+	if (OS_IOS) {
+		var version = Titanium.Platform.version.split(".");
+		var major = parseInt(version[0], 10);
+		if (major >= 7) {
+			return true;
+		}
+	}
+	return false;
+};
+
+Alloy.Globals.iOS7Plus = isIOS7Plus();
+
+Alloy.Collections.menuItems = new Backbone.Collection();
