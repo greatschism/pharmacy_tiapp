be6b07abc3d75582378dd17a6487e2dd94c30ef0
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-09-16 12:21:06 +0000

    store detail

--- app/controllers/storeDetail.js
+++ app/controllers/storeDetail.js
@@ -1,8 +1,36 @@
-var args = arguments[0] || {}, App = require("core");
+var args = arguments[0] || {}, App = require("core"), moment = require("alloy/moment");
 
-Alloy.Models.store.set(Alloy.Collections.stores.where({
-storeid: args.storeId
-})[0].toJSON());
+(function() {
+	var store = Alloy.Collections.stores.where({
+	storeid: args.storeId
+	})[0].toJSON();
+	var dates = store.hours.date;
+	var date = _.findWhere(dates, {
+		day : moment().format("dddd")
+	});
+	if (date) {
+		var storehour = String(date.storehours);
+		var till = storehour.substring(storehour.indexOf("-") + 1, storehour.length);
+		var toClose = moment(till, "h:mm A").diff(moment(), "minutes");
+		var image, labelDict;
+		if (toClose > 0) {
+			image = "/images/store/clock_open.png";
+			labelDict = {
+				text : "Open till ".concat(till),
+				color : "#245e3d"
+			};
+		} else {
+			image = "/images/store/clock_close.png";
+			labelDict = {
+				text : "Closed at ".concat(till),
+				color : "#610e07"
+			};
+		}
+		$.clockImg.image = image;
+		$.clockLbl.applyProperties(labelDict);
+	}
+	Alloy.Models.store.set(store);
+})();
 
 function terminate() {
 	$.destroy();
@@ -20,4 +48,4 @@ function didRefill(e) {
 	});
 }
 
-exports.terminate = terminate; 
+exports.terminate = terminate;
