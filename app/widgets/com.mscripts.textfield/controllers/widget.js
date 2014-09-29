4e6ff5d9658120f959f62f7800195c53f73bc24e
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-09-29 07:08:03 +0000

    textfield widget - rightButton support

--- app/widgets/com.mscripts.textfield/controllers/widget.js
+++ app/widgets/com.mscripts.textfield/controllers/widget.js
@@ -4,40 +4,89 @@ var args = arguments[0] || {};
 
 	var options = {};
 
-	var className = args.search && (args.clearButton || args.rightImage) ? "left-right" : (args.search ? "left" : (args.clearButton || args.rightImage ? "right" : "fill"));
-	$.addClass($.container, "container-" + className);
+	var cls = "fill";
+	if (args.search && (args.clearButton || args.rightImag || args.rightButtonTitle)) {
+		cls = "left-right";
+	} else if (args.search) {
+		cls = "left";
+	} else if (args.clearButton || args.rightImag || args.rightButtonTitle) {
+		cls = "right";
+	}
+
+	$.addClass($.container, "container-" + cls);
+
 	$.txt = $.UI.create("TextField", {
 		apiName : "TextField",
-		classes : ["txt", ("txt-" + className)],
+		classes : [("txt-" + cls)],
 		id : "txt"
 	});
 	$.txt.addEventListener("focus", didFocus);
 	$.txt.addEventListener("blur", didBlur);
 	$.txt.addEventListener("return", didReturn);
+	$.txt.addEventListener("change", didChange);
 	$.container.add($.txt);
 
 	if (args.search == true) {
+
 		console.debug("adding search icon");
-		$.widget.add(Widget.createController("search").getView());
+
+		$.widget.add($.UI.create("ImageView", {
+			apiName : "ImageView",
+			id : "search"
+		}));
 	}
 
 	if (args.clearButton == true || _.has(args, "rightImage")) {
-		console.debug("adding right butotn");
-		$.rightImg = Widget.createController("rightImg", {
-			image : args.clearButton ? WPATH("cancel.png") : args.rightImage
-		}).getView();
+
+		console.debug("adding right image");
+
+		$.rightImg = $.UI.create("ImageView", {
+			apiName : "ImageView",
+			id : "rightImg"
+		});
+
+		$.rightImg.image = args.clearButton ? WPATH("cancel.png") : args.rightImage;
+
+		if (args.rightImageWidth) {
+			$.rightImg.width = args.rightImageWidth;
+			$.txt.right = OS_MOBILEWEB ? ($.rightImg.width + 10) : $.rightImg.width;
+		}
+
 		$.container.add($.rightImg);
+
 		var listener;
 		if (args.clearButton == true) {
-			$.txt.addEventListener("change", didChange);
 			listener = didClear;
 		} else {
 			$.rightImg.visible = true;
 			listener = didClick;
 		}
+
 		$.rightImg.addEventListener("singletap", listener);
+
+	} else if (_.has(args, "rightButtonTitle")) {
+
+		console.debug("adding right button");
+
+		$.rightBtn = $.UI.create("Button", {
+			apiName : "Button",
+			id : "rightBtn"
+		});
+		$.rightBtn.applyProperties({
+			title : args.rightButtonTitle,
+			width : args.rightButtonWidth || 50,
+			font : args.font || {
+				fontSize : 12
+			},
+			color : args.rightButtonColor || "#000"
+		});
+		$.container.add($.rightBtn);
+
+		$.txt.right = $.rightBtn.width + ( OS_MOBILEWEB ? 20 : 10);
+
+		$.rightBtn.addEventListener("click", didClick);
 	}
-	
+
 	options = _.pick(args, ["width", "height", "top", "bottom", "left", "right", "backgroundColor", "borderColor", "borderWidth", "borderRadius"]);
 	if (!_.isEmpty(options)) {
 		$.widget.applyProperties(options);
@@ -59,7 +108,10 @@ function didBlur(e) {
 }
 
 function didChange(e) {
-	$.rightImg.visible = $.txt.getValue() != "";
+	$.trigger("change");
+	if (args.clearButton == true) {
+		$.rightImg.visible = $.txt.getValue() != "";
+	}
 }
 
 function didReturn(e) {
@@ -88,6 +140,9 @@ exports.blur = function() {
 
 exports.setValue = function(value) {
 	$.txt.setValue(value);
+	if (args.clearButton == true) {
+		$.rightImg.visible = $.txt.getValue() != "";
+	}
 };
 
 exports.getValue = function() {
