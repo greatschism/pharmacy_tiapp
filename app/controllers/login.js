9081f51d96bbb3486b8a0b50782e29b74b5a8994
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-10-01 10:34:05 +0000

    forgot password link inside password field

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,7 +1,5 @@
 var args = arguments[0] || {};
 
 function didRightclickPwd(e) {
-	var passwordMask = $.passwordTxt.getPasswordMask();
-	$.passwordTxt.setPasswordMask(!passwordMask);
-	$.passwordTxt.setRightImage(("/images/login/" + ( passwordMask ? "check" : "question") + "_mark.png"));
+	alert("forgot password");
 }
