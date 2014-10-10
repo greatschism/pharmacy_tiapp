dd80b718d2f998c962236a3d1ca05865cc46b675
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-10-10 06:53:04 +0000

    doctors

--- app/controllers/doctors.js
+++ app/controllers/doctors.js
@@ -1,14 +1,9 @@
 var args = arguments[0] || {};
 
-$.toggleImg.image = "/images/ic_action_overflow.png";
-
 function didToggle(e) {
 
 }
 
+function didItemClick(e) {
 
-
-function didItemClick(e)
-{
-	
 }
