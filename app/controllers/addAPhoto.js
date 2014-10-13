52d099a4ddce6ef00be3df5a33bd1d76c58d342e
pkulkarni <pkulkarni@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-10-13 08:44:51 +0000

    Added function to open camera.

--- app/controllers/addAPhoto.js
+++ app/controllers/addAPhoto.js
@@ -1,1 +1,21 @@
 var args = arguments[0] || {};
+
+
+
+
+
+
+
+Ti.Media.showCamera({
+		success : function(e) {
+			Ti.API.info("success");
+		},
+		cancel : function() {
+			Ti.API.info("error");
+		},
+		error : function(error) {
+			Ti.API.info("error: " + JSON.stringify(error));
+		},
+		autohide : false,
+		showControls : true
+	});
--- app/controllers/doctors.js
+++ app/controllers/doctors.js
@@ -7,3 +7,52 @@ function didToggle(e) {
 function didItemClick(e) {
 
 }
+
+function OpenCamera(e) {
+	var win = Ti.UI.createWindow({
+		backgroundColor : "#00ff00"
+	});
+
+	win.addEventListener("open", function() {
+		Titanium.Media.showCamera({
+
+			success : function(event) {
+				var cropRect = event.cropRect;
+				var image = event.media;
+
+				Ti.API.debug('Our type was: ' + event.mediaType);
+				if (event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
+					anImageView.image = image;
+					//new_upload_profile_picture_update(anImageView.image);
+				} else {
+					alert("got the wrong type back =" + event.mediaType);
+				}
+			},
+			cancel : function() {
+			},
+			error : function(error) {
+				// create alert
+				var a = Titanium.UI.createAlertDialog({
+					title : 'Camera'
+				});
+
+				// set message
+				if (error.code == Titanium.Media.NO_CAMERA) {
+					a.setMessage('Please run this test on device');
+				} else {
+					a.setMessage('Unexpected error: ' + error.code);
+				}
+
+				// show alert
+				a.show();
+			},
+			saveToPhotoGallery : true,
+			allowEditing : false,
+			
+			mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO]
+		});
+	});
+
+	win.open();
+}
+
