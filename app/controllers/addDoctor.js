b4a6b07158029ff72632305e6073934f7b3877bc
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-10-16 12:36:18 +0000

    add doctor fixes

--- app/controllers/addDoctor.js
+++ app/controllers/addDoctor.js
@@ -1,15 +1,7 @@
-var args = arguments[0] || {};
-App = require("core");
+var args = arguments[0] || {},
+    App = require("core");
 
 function moveToNext(e) {
 	var nextItem = e.nextItem || "";
 	$[nextItem] && $[nextItem].focus();
-}
-
-function handleScroll(e) {
-	$.scrollView.canCancelEvents = e.value;
-}
-
-function chooseLocation(e) {
-
 }
