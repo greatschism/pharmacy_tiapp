38faaedbdac2fbb88d125b692b2f56738d10c488
pkulkarni <pkulkarni@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-11-07 06:54:42 +0000

    Code cleanup.

--- app/controllers/prescriptionDetails.js
+++ app/controllers/prescriptionDetails.js
@@ -1,1 +1,28 @@
-var args = arguments[0] || {};
+var args = arguments[0] || {},
+    app = require("core"),
+    moment = require("alloy/moment");
+
+	Alloy.Collections.prescriptionDetails.reset([{
+
+		desc : "Tramadol HCL, 20mg tab qual",
+		time : "Order placed; should be ready by Tuesday 2PM."
+	}]);
+
+
+function terminate() {
+	$.destroy();
+	Alloy.Models.store.clear();
+}
+
+function didRefill(e) {
+	app.navigator.open({
+		ctrl : "refill",
+		titleid : "titleOrderRefill",
+		stack : true,
+		ctrlArguments : {
+			message : "Refill"
+		}
+	});
+}
+
+exports.terminate = terminate;
