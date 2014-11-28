21e2f77b23cdd343c96422b0010f17e878ced4e3
dnandakumar <dnandakumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2014-11-28 13:51:31 +0000

    Added "About" info to the application

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -1,1 +1,14 @@
 var args = arguments[0] || {};
+
+var didClickAbout = function()
+{
+	 var dialog = Ti.UI.createAlertDialog({
+			    message: 'Powered by mscripts \n' + 
+			    		 "Build Version: " + Alloy.CFG.buildVersion + "\n" + 
+			    		 "Build Date: " + Alloy.CFG.buildDate + "\n" + 
+			    		 "Build Number: " + Alloy.CFG.buildNumber,
+			    ok: 'Ok',
+			    title: Alloy.Globals.Strings.alertTitleAbout
+			  });
+	 dialog.show();
+};
