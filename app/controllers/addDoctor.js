cf9f823a411fba6b17a9b3762be5e897dbfe1e60
pkulkarni <pkulkarni@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-01-02 07:11:05 +0000

    No change.

--- app/controllers/addDoctor.js
+++ app/controllers/addDoctor.js
@@ -1,5 +1,9 @@
 var args = arguments[0] || {},
-    app = require("core");
+   app = require("core"),
+    dialog = require("dialog"),
+    locale = require("localization"),
+    languages = locale.getLanguages(),
+    lngStrs = Alloy.Globals.strings;
 
 function moveToNext(e) {
 	var nextItem = e.nextItem || "";
@@ -9,3 +13,78 @@ function moveToNext(e) {
 function didClickSave(e) {
 
 }
+
+
+
+function userVal(e) {
+ // var nameRegex = /^[a-zA-Z\-]+$/;
+// var validfirstUsername = $.fnameTxt.value.replace(/^[a-zA-Z\-]+$/); 
+     // var validfirstUsername = $.fnameTxt.value.match(nameRegex);
+   // if(validfirstUsername == null){
+        // alert("Your first name is not valid. Only characters A-Z, a-z and '-' are  acceptable.");
+       // $.fnameTxt.focus();
+        // return false;
+     // }
+
+}
+
+
+function numVal(e)
+{
+	  //  e.source.value = e.source.value.replace(/[^0-9]+/,""); 
+}
+
+function didStateChange(e)
+{
+//	locale.setLanguage($.stateTxt.getSelectedItem().code);
+//	Alloy.Collections.menuItems.trigger("reset");
+//	app.navigator.open({
+//		titleid : "titleHome",
+//		ctrl : "home"
+//	});
+}
+
+
+
+  
+
+function init() {
+	// var templates = [],
+	    // lngs = [],
+	    // i = 0,
+	    // selectedIndex = 0;
+	// _.each(Alloy.Globals.homePageTemplates, function(obj) {
+		// var template = _.pick(obj, ["title"]);
+		// template.index = i;
+		// templates.push(template);
+		// i++;
+	// });
+// 	
+	// i = 0;
+	// _.each(languages, function(obj) {
+		// var lng = _.pick(obj, ["titleid", "code"]);
+		// _.extend(lng, {
+			// title : lngStrs[lng.titleid]
+		// });
+		// lngs.push(lng);
+		// if (obj.selected) {
+			// selectedIndex = i;
+		// }
+		// i++;
+	// });
+	// $.stateTxt.setChoices(lngs);
+	// $.stateTxt.setSelectedIndex(selectedIndex);
+}
+
+function setParentViews(view) {
+
+//	$.stateTxt.setParentView(view);
+}
+
+
+
+
+
+exports.init = init;
+exports.setParentViews = setParentViews;
+
