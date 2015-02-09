157b467dd0f13695ef1f3b75901f8445edec5656
vdassani <vdassani@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-02-09 12:46:56 +0000

    Added new String value for title. Added new API names in Config.json Created new screen mobileEnterOTP.

---
+++ app/controllers/mobileEnterOTP.js
@@ -0,0 +1,16 @@
+var args = arguments[0] || {};
+
+var mobileNumber = args[0];
+
+// http.request({
+// method : "PATIENTS_MOBILE_EXISTS_OR_SHARED", // This value is defined in config.json
+// forceRetry : false,
+// data : {
+// data : {
+// patient : "",
+// mobile_number : mobileNumber
+// }
+// },
+// success : didGetResponse
+// });
+
--- app/controllers/mobileNumber.js
+++ app/controllers/mobileNumber.js
@@ -1,10 +1,77 @@
 var args = arguments[0] || {},
-    app = require("core");
+    app = require("core"),
+    http = require("requestwrapper");
 
 function didClickContinue(e) {
-	app.navigator.open({
-		ctrl : "fullSignup",
-		titleid: "strSignup",
-		stack : true
+
+	// app.navigator.open({
+	// ctrl : "fullSignup",
+	// titleid: "strSignup",
+	// stack : true
+	// });
+
+	http.request({
+		method : "PATIENTS_MOBILE_EXISTS_OR_SHARED", // This value is defined in config.json
+		forceRetry : false,
+		data : {
+			data : {
+				patient : "",
+				mobile_number : "x"
+			}
+		},
+		success : didGetResponse
 	});
+
+}
+
+function didGetResponse(result) {
+	var mobileNumber = $.mobileTxt.getValue();
+
+	var isMobileShared = result.data.is_mobile_shared,
+	    mobileExists = result.data.mobile_exists;
+	if (mobileExists !== 1) {
+		app.navigator.open({
+			ctrl : "fullSignup",
+			titleid : "strSignup",
+			stack : true,
+			ctrlArguments : {
+				firstName : mobileExists,
+				dateOfBirth : ""
+			}
+		});
+	} else {
+		if (isMobileShared !== 1) {
+
+			// http.request({
+			// method : "PATIENTS_MOBILE_EXISTS_OR_SHARED", // This value is defined in config.json
+			// forceRetry : false,
+			// data : {
+			// data : {
+			// patient : "",
+			// mobile_number : mobileNumber
+			// }
+			// },
+			// success : didGetResponse
+			// });
+			app.navigator.open({
+				ctrl : "mobileEnterOTP",
+				titleid : "",
+				stack : true,
+				ctrlArguments : {
+					firstName : "",
+					dateOfBirth : "",
+					mobileNumber : mobileNumber
+				}
+			});
+		} else {
+			app.navigator.open({
+				ctrl : "sharedMobileCheck",
+				titleid : "titleSharedMobileNumber",
+				stack : true,
+				ctrlArguments : {
+					mobileNumber : mobileNumber
+				}
+			});
+		}
+	}
 }
