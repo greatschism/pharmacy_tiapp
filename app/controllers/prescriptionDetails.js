1d7a4313e8cbb5a21bd5f79d403d1b9de4728276
pkulkarni <pkulkarni@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-02-23 11:36:06 +0000

    Added click listener to navigate order details.

--- app/controllers/prescriptionDetails.js
+++ app/controllers/prescriptionDetails.js
@@ -1,8 +1,10 @@
 var args = arguments[0] || {},
-    moment = require("alloy/moment");
+    moment = require("alloy/moment"),
+    app = require("core"),
+    prescription;
 
 function init() {
-	var prescription = args.prescription || {};
+	prescription = args.prescription || {};
 	$.prescriptionNameLbl.text = prescription.presc_name;
 	$.refillLeftInfoLbl.text = prescription.refill_remaining_preferences;
 	$.dueForRefillInfoLbl.text = moment(prescription.anticipated_refill_date, "YYYY/MM/DD").format("D/M/YY");
@@ -11,5 +13,21 @@ function init() {
 	$.remindMeToRefillSwt.setValue(true);
 	$.setTimeSwt.setValue(true);
 }
+
+
+function didClickRefillPrescription()
+{
+	app.navigator.open({
+			ctrl : "orderDetails",
+			titleid : "titleOrderDetails",
+			ctrlArguments : {
+				prescription : prescription.presc_name
+			},
+			stack : true
+	});
+}
+
+
+
 
 exports.init = init;
