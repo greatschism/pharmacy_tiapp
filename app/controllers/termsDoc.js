873aa1d22f61da5d43a6f5b3ed8d1ea89862be54
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-02-27 11:48:20 +0000

    termsdoc

--- app/controllers/termsDoc.js
+++ app/controllers/termsDoc.js
@@ -2,21 +2,11 @@ var args = arguments[0] || {},
     app = require("core");
 
 function init() {
-	if (args.agreement_name == "HIPAA") {
-		$.addClass($.scrollView, ["vgroup"]);
-		$.webView.applyProperties({
-			top : 0,
-			height : Ti.UI.SIZE,
-			url : args.agreement_url
-		});
-	} else {
-		$.addClass($.footerView, ["bottom"]);
-		$.webView.applyProperties({
-			top : 0,
-			bottom : Alloy.TSS.btn_padding_top.top + Alloy.TSS.margin_bottom.bottom + Alloy.TSS.primary_btn.height,
-			url : args.agreement_url
-		});
-	}
+	$.webView.applyProperties({
+		top : 0,
+		bottom : Alloy.TSS.btn_padding_top.top + Alloy.TSS.margin_bottom.bottom + Alloy.TSS.primary_btn.height,
+		url : args.agreement_url
+	});
 }
 
 function didClickDone(e) {
