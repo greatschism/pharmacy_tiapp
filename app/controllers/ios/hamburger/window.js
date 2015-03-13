f7a98f7b1c46956f9c3e209d9a27a2e76f2dd778
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-03-13 13:37:34 +0000

    ios - complete rebuild of navigation module with windows (Yet to work on android - Android app may break with the update)

---
+++ app/controllers/ios/hamburger/window.js
@@ -0,0 +1,103 @@
+var args = arguments[0] || {},
+    app = require("core"),
+    controller;
+
+(function() {
+
+	var dict = {};
+	if (args.navBarHidden) {
+		dict.navBarHidden = true;
+	}
+
+	dict.title = args.title || Alloy.Globals.strings[args.titleid || ""] || "";
+
+	$.leftNavView = Ti.UI.createView();
+
+	if (args.stack) {
+		app.navigator.drawer.setOpenDrawerGestureMode(app.navigator.drawer.OPEN_MODE_NONE);
+		$.leftNavBtn = $.UI.create("Button", {
+			classes : ["nav-icon-btn"],
+			title : Alloy.CFG.icons.back,
+			accessibilityLabel : Alloy.Globals.strings.accessibilityLblBack
+		});
+	} else {
+		app.navigator.drawer.setOpenDrawerGestureMode(app.navigator.drawer.OPEN_MODE_BEZEL_PANNING_CENTERWINDOW);
+		$.leftNavBtn = $.UI.create("Button", {
+			classes : ["nav-icon-btn"],
+			title : Alloy.CFG.icons.hamburger,
+			accessibilityLabel : Alloy.Globals.strings.accessibilityLblMenu
+		});
+	}
+
+	$.leftNavView.addEventListener("click", didClickLeftNavView);
+
+	$.leftNavView.add($.leftNavBtn);
+
+	dict.leftNavButton = $.leftNavView;
+
+	//reload tss of this controller in memory
+	require("config").updateTSS(args.ctrl);
+
+	controller = Alloy.createController(args.ctrl, args.ctrlArguments || {});
+
+	_.each(controller.getTopLevelViews(), function(child) {
+		if (child.__controllerPath) {
+			child = child.getView();
+		}
+		switch(child.role) {
+		case "rightNavButton":
+			dict.rightNavButton = child;
+			break;
+		default:
+			$.window.add(child);
+		}
+	});
+
+	$.window.applyProperties(dict);
+
+	controller.app = app;
+
+	controller.hideNavBar = hideNavBar;
+
+	controller.hideNavBar = hideNavBar;
+
+	_.isFunction(controller.init) && controller.init();
+
+	_.isFunction(controller.setParentViews) && controller.setParentViews($.window);
+
+})();
+
+function didOpen(e) {
+	_.isFunction(controller.focus) && controller.focus();
+}
+
+function didBlur(e) {
+	_.isFunction(controller.blur) && controller.blur();
+}
+
+function didClose(e) {
+	_.isFunction(controller.terminate) && controller.terminate();
+}
+
+function didClickLeftNavView(e) {
+	if (args.stack) {
+		if (_.isFunction(controller.backButtonHandler) && controller.backButtonHandler()) {
+			return;
+		}
+		app.navigator.close();
+	} else {
+		app.navigator.drawer.toggleLeftWindow();
+	}
+}
+
+function showNavBar(_animated) {
+	$.window.showNavBar({
+		animated : _.isUndefined(_animated) ? true : false
+	});
+}
+
+function hideNavBar(_animated) {
+	$.window.hideNavBar({
+		animated : _.isUndefined(_animated) ? true : false
+	});
+}
