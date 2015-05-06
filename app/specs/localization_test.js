904bebaec9c328a60a3d27b2206e35ef36ddbe04
kpai <kpai@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-05-06 09:27:21 +0000

    Committing localization test cases

--- app/specs/localization_test.js
+++ app/specs/localization_test.js
@@ -1,2 +1,11 @@
-var localization=require('localization');
+var localization = require('localization');
 var alloy = require("alloy");
+var currentLanguage = localization.currentLanguage.id;
+describe("Localization Test Suite", function() {
+	it("Get the current language - Success (Test Case 1)", function() {
+		currentLanguage.should.be.equal("en");
+	});
+	it("Get the current language - Failure (Test Case 2)", function() {
+		currentLanguage.should.be.equal("es");
+	});
+});
