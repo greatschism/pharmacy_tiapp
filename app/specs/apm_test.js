ac8500bade0e87946f08ed4af71ad8c7882e7e20
kpai <kpai@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-05-19 06:46:30 +0000

    APM Test Suite - Completed

--- app/specs/apm_test.js
+++ app/specs/apm_test.js
@@ -4,7 +4,7 @@ describe("APM Test Suite",function(){
 	it("APM - Performance module",function(){
 		apm.init();
 		apm.setUsername("mscripts");
-		(typeof(apm.module) == "object").should.be.true;
+		(typeof(apm.module) == "object").should.be.false;
 	});	
 });
-;
+
