0c15e3d2757441f26ee1d351c6b4a16a317eab4c
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-05-20 13:51:23 +0000

    updated unit test

--- app/specs/encryptionUtil_test.js
+++ app/specs/encryptionUtil_test.js
@@ -10,4 +10,5 @@ describe("EncryptionUtil Test Suite", function() {
 	it("Test Case 2: decrypt with invalid string", function() {
 		encryptionUtil.decrypt("test string").should.be.equal("");
 	});
+	
 });
