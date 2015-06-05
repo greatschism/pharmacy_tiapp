8b6a136a35628e7c6fb300da2695381dcdc34275
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-06-05 13:30:54 +0000

    plugin fixes

--- plugins/ti.build.properties/cli/hooks/builder.js
+++ plugins/ti.build.properties/cli/hooks/builder.js
@@ -4,20 +4,15 @@ exports.cliVersion = ">=3.X";
 var TAG = "PropertiesBuilder",
     fs = require("fs");
 
-/**
- * The 3.0+ CLI doesn"t properly pass the deploy type for Android,
- * so get it based on the target
- */
-function mapTargetToDeployType(target) {
+function checkIfProduction(target) {
 	switch(target) {
+	case "production":
+	case "dist-adhoc":
+	case "dist-appstore":
 	case "dist-playstore":
-		return "production";
-	case "device":
-		return "test";
-	case "emulator":
-		return "development";
+		return true;
 	default:
-		return "development";
+		return false;
 	}
 }
 
@@ -31,66 +26,70 @@ exports.init = function(logger, config, cli, appc) {
 
 	cli.on("build.pre.construct", function(build, done) {
 
-		var deployType = mapTargetToDeployType(cli.argv["target"]);
+		var isProd = checkIfProduction(cli.argv["target"]);
 
 		try {
 
-			if (deployType == "production") {
+			if (isProd) {
 
 				//update tiapp.xml
 				var tiappPath = projectDir + "/tiapp.xml",
 				    tiapp = require("tiapp.xml").load(tiappPath);
 
 				//ios properties
-				var iosTags = tiapp.doc.documentElement.getElementsByTagName("ios"),
-				    ios = iosTags.length == 0 ? tiapp.doc.documentElement.appendChild(tiapp.doc.createElement("ios")) : iosTags[0];
+				if (cli.argv["platform"] === "ios") {
+					var iosTags = tiapp.doc.documentElement.getElementsByTagName("ios"),
+					    ios = iosTags.length == 0 ? tiapp.doc.documentElement.appendChild(tiapp.doc.createElement("ios")) : iosTags[0];
 
-				//minimum SDK version
-				var minOSTags = ios.getElementsByTagName("min-ios-ver"),
-				    minOS = minOSTags.length == 0 ? ios.appendChild(tiapp.doc.createElement("min-ios-ver")) : minOSTags[0];
-				if (minOS.hasChildNodes()) {
-					for (var i in minOS.childNodes) {
-						minOS.removeChild(minOS.childNodes[i]);
+					//minimum SDK version
+					var minOSTags = ios.getElementsByTagName("min-ios-ver"),
+					    minOS = minOSTags.length == 0 ? ios.appendChild(tiapp.doc.createElement("min-ios-ver")) : minOSTags[0];
+					if (minOS.hasChildNodes()) {
+						for (var i in minOS.childNodes) {
+							minOS.removeChild(minOS.childNodes[i]);
+						}
 					}
+					minOS.appendChild(tiapp.doc.createTextNode(cli.argv["minSdkVersion"] || "7.1"));
 				}
-				minOS.appendChild(tiapp.doc.createTextNode(cli.argv["minSdkVersion"] || "7.1"));
 
 				//android properties
-				var androidTags = tiapp.doc.documentElement.getElementsByTagName("android"),
-				    android = androidTags.length == 0 ? tiapp.doc.documentElement.appendChild(tiapp.doc.createElement("android")) : androidTags[0];
-				if (!android.hasAttribute("xmlns:android")) {
-					android.setAttribute("xmlns:android", "http://schemas.android.com/apk/res/android");
-				}
+				if (cli.argv["platform"] === "android") {
+					var androidTags = tiapp.doc.documentElement.getElementsByTagName("android"),
+					    android = androidTags.length == 0 ? tiapp.doc.documentElement.appendChild(tiapp.doc.createElement("android")) : androidTags[0];
+					if (!android.hasAttribute("xmlns:android")) {
+						android.setAttribute("xmlns:android", "http://schemas.android.com/apk/res/android");
+					}
 
-				//version code
-				var manifestTags = android.getElementsByTagName("manifest"),
-				    manifest = manifestTags.length == 0 ? android.appendChild(tiapp.doc.createElement("manifest")) : manifestTags[0],
-				    versionCode = cli.argv["versionCode"];
-				if (!versionCode) {
-					versionCode = parseInt(manifest.getAttribute("android:versionCode")) || 0;
-					versionCode += 1;
-				}
-				manifest.setAttribute("android:versionCode", versionCode);
+					//version code
+					var manifestTags = android.getElementsByTagName("manifest"),
+					    manifest = manifestTags.length == 0 ? android.appendChild(tiapp.doc.createElement("manifest")) : manifestTags[0],
+					    versionCode = cli.argv["versionCode"];
+					if (!versionCode) {
+						versionCode = parseInt(manifest.getAttribute("android:versionCode")) || 0;
+						versionCode += 1;
+					}
+					manifest.setAttribute("android:versionCode", versionCode);
 
-				//minimum SDK version
-				var usesSDKTags = android.getElementsByTagName("uses-sdk"),
-				    usesSDK = usesSDKTags.length == 0 ? manifest.appendChild(tiapp.doc.createElement("uses-sdk")) : usesSDKTags[0];
-				usesSDK.setAttribute("android:minSdkVersion", cli.argv["minSdkVersion"] || "14");
+					//minimum SDK version
+					var usesSDKTags = android.getElementsByTagName("uses-sdk"),
+					    usesSDK = usesSDKTags.length == 0 ? manifest.appendChild(tiapp.doc.createElement("uses-sdk")) : usesSDKTags[0];
+					usesSDK.setAttribute("android:minSdkVersion", cli.argv["minSdkVersion"] || "14");
 
-				//launching activity name
-				var application = android.getElementsByTagName("application")[0];
-				if (application) {
-					var activity = android.getElementsByTagName("activity")[0];
-					if (activity) {
-						var names = (tiapp.name + " Activity").split(" ");
-						for (var i in names) {
-							var name = names[i].toLowerCase();
-							name = (name + '').replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function($1) {
-								return $1.toUpperCase();
-							});
-							names[i] = name;
+					//launching activity name
+					var application = android.getElementsByTagName("application")[0];
+					if (application) {
+						var activity = android.getElementsByTagName("activity")[0];
+						if (activity) {
+							var names = (tiapp.name + " Activity").split(" ");
+							for (var i in names) {
+								var name = names[i].toLowerCase();
+								name = (name + '').replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function($1) {
+									return $1.toUpperCase();
+								});
+								names[i] = name;
+							}
+							activity.setAttribute("android:name", "." + names.join(""));
 						}
-						activity.setAttribute("android:name", "." + names.join(""));
 					}
 				}
 
@@ -110,6 +109,7 @@ exports.init = function(logger, config, cli, appc) {
 						tiapp.version = versions.join(".");
 					}
 				}
+
 				tiapp.write();
 
 				//update config
