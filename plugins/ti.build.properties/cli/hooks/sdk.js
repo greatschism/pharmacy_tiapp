f63bcd22b26d8e03c95c74aedfbb87c302de5088
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-07-07 06:33:58 +0000

    added comments

--- plugins/ti.build.properties/cli/hooks/sdk.js
+++ plugins/ti.build.properties/cli/hooks/sdk.js
@@ -1,3 +1,7 @@
-//get tiapp.xml
+/**
+ * Usage: node PATH_TO_PROJECT/plugins/ti.build.properties/cli/hooks/sdk.js
+ * returns the sdk-version value form tiapp.xml
+ * Executed by Jenkins to pickup the right sdk version for ti build command.
+ */
 var tiapp = require("tiapp.xml").load(__dirname + "/../../../../tiapp.xml");
 console.log(tiapp.sdkVersion);
