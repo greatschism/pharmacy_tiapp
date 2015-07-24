7d9fddedaaacb46ea1b424357d93e248e0873a0b
kpai <kpai@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-07-24 12:46:03 +0000

    PHA-860 - Worked on the UI and validations  for Type transfer rx screen.

--- app/controllers/transferRxTypePresc.js
+++ app/controllers/transferRxTypePresc.js
@@ -1,12 +1,82 @@
+var args = arguments[0] || {},
+    apiCodes = Alloy.CFG.apiCodes;
+
+function init() {
+	getOriginalPharmacies();
+}
+
+function getOriginalPharmacies() {
+	$.http.request({
+		method : "codes_get",
+		params : {
+			feature_code : "THXXX",
+			data : [{
+				codes : [{
+					code_name : apiCodes.code_original_pharmacy
+				}]
+			}]
+		},
+		success : didGetOriginalPharmacies,
+
+	});
+
+}
+
+function didGetOriginalPharmacies(result) {
+	Alloy.Models.originalPharmacies.set(result.data.codes[0]);
+	var codes = Alloy.Models.originalPharmacies.get("code_values"),
+	    defaultVal = Alloy.Models.originalPharmacies.get("default_value");
+	_.each(codes, function(code) {
+		if (code.code_value === defaultVal) {
+			Alloy.Models.originalPharmacies.set("selected_code_value", code.code_value);
+			code.selected = true;
+		} else {
+			code.selected = false;
+		}
+	});
+	console.log(codes);
+
+}
+
+function didChange(e) {
+	var value = $.utilities.formatPhoneNumber(e.value),
+	    len = value.length;
+	$.phoneNumberTxt.setValue(value);
+	$.phoneNumberTxt.setSelection(len, len);
+}
+
+function didClickNext() {
+	var prescName = $.prescNameTxt.getValue(),
+	    phoneNumber = $.phoneNumberTxt.getValue();
+	if (!prescName) {
+		$.uihelper.showDialog({
+			message : $.strings.transferRxTypePrescValPrescName
+		});
+		return;
+	}
+	if (!phoneNumber) {
+		$.uihelper.showDialog({
+			message : $.strings.transferRxTypePrescValPhone
+		});
+		return;
+	}
+	phoneNumber = $.utilities.validatePhoneNumber(phoneNumber);
+	if (!phoneNumber) {
+		$.uihelper.showDialog({
+			message : $.strings.transferRxTypePrescValPhoneInvalid
+		});
+		return;
+	}
+	
+	
+}
+
 function moveToNext(e) {
 	var nextItem = e.nextItem || false;
 	if (nextItem && $[nextItem]) {
 		$[nextItem].focus();
 	}
 }
-function didClickPharmacyTransferFromPickerClose(e) {
-	$.pharmacyTransferFromPicker.hide();
-}
-function updatePharmacyTransferPicker() {
-	
-}
+
+exports.init = init;
+
