06943fbe6246c8181bdf553858f9e02deb89b3e5
kpai <kpai@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-07-31 09:00:53 +0000

    Committing success screen changes

--- app/controllers/transferSuccess.js
+++ app/controllers/transferSuccess.js
@@ -1,15 +1,36 @@
-var args = arguments[0] || {};
-console.log(args);
+var args = arguments[0] || {},
+stores,
+phone_formatted;
 
+function didGetStore(result){
+	stores=result.data.stores;
+	phone_formatted=$.utilities.formatPhoneNumber(stores.phone);
+	$.storePhoneAttr.setHtml(String.format($.strings.attrPhone, phone_formatted));
+}
 function focus() {
-	$.uihelper.getImage("success",$.successImg);
-	store=args.store;
-	prescription=args.prescription;
-	$.storeNameLbl.text=store.storeName;
-	$.storeTitleLbl.text=store.title;
-	$.storeSubtitleLbl.text=store.subtitle;
+	store = args.store;
+	if(!store.phone_formatted){
+		$.http.request({
+			method : "stores_get",
+			params : {
+				feature_code : "THXXX",
+				data : [{
+					stores : {
+						id : store.id,
+					}
+				}]
+			},
+			forceRetry : true,
+			success : didGetStore
+		});
+	}
+	$.uihelper.getImage("success", $.successImg);
+	prescription = args.prescription;
+	$.storeNameLbl.text = $.utilities.ucword(store.store_name);
+	$.storeTitleLbl.text = store.title;
+	$.storeSubtitleLbl.text = store.subtitle;
 	$.storePhoneAttr.setHtml(String.format($.strings.attrPhone, store.phone_formatted));
-		if (prescription) {
+	if (prescription) {
 		$.prescNameLbl.text = prescription.name;
 		if (prescription.rx) {
 			$.rxLbl.text = prescription.rx;
@@ -30,7 +51,7 @@ function focus() {
 		 */
 		$.prescImg.image = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, "transfer.jpg").read();
 	}
-	
+
 }
 
 function didClickPhone(e) {
@@ -42,12 +63,10 @@ function didClickPhone(e) {
 	}, store.phone);
 }
 
-
-
 function didClickDone(e) {
 	$.app.navigator.open(Alloy.Collections.menuItems.findWhere({
 		landing_page : true
 	}).toJSON());
 }
 
-exports.focus = focus;
+exports.focus = focus; 
