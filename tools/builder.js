ddf7fc209c0feeec25f1452c7d35a527b5e4eac3
mkumar <mkumar@6368d97f-c38d-46b8-81a9-5aff22a28fb4>
2015-10-01 06:15:57 +0000

    builder minor bug fixes

--- tools/builder.js
+++ tools/builder.js
@@ -1,5 +1,4 @@
 var log4js = require("log4js"),
-    logger = log4js.getLogger("Builder"),
     program = require("commander"),
     path = require("path"),
     fs = require("fs-extra"),
@@ -16,7 +15,7 @@ var log4js = require("log4js"),
     DEFAULT_ENVIRONMENT = "dev",
     DEFAULT_SDK_VERSION = "4.1.0.GA",
     DEFAULT_APP_VERSION = "7.0.0",
-    DEFAULT_APP_VERSION_CODE = 1,
+    DEFAULT_APP_VERSION_CODE = "1",
     DEFAULT_PLATFORM = "ios",
     DEFAULT_TARGET = "dist-adhoc",
     DEFAULT_OUTPUT_DIR = ROOT_DIR + "dist",
@@ -37,7 +36,8 @@ var log4js = require("log4js"),
     CLEINT_RESOURCE_BASE_DIR,
     CLEINT_KEYS_BASE_DIR,
     CLIENT_ENV_JSON,
-    CLIENT_BASE_THEME;
+    CLIENT_BASE_THEME,
+    logger;
 
 /**
  * build program
@@ -45,25 +45,33 @@ var log4js = require("log4js"),
  */
 program.option("-c, --client-identifier <client-identifier>", "Client identifier to build with; should match with any one defined in client.json", DEFAULT_CLIENT_IDENTIFIER);
 program.option("-e, --environment <environment>", "Environment to build with; should match with any one defined in env.json", toLowerCase, DEFAULT_ENVIRONMENT);
-program.option("--sdk-version <version>", "SDK version", DEFAULT_SDK_VERSION);
+program.option("-s, --sdk <version>", "Titanium SDK version to build with. Defaults to " + DEFAULT_SDK_VERSION + ".", DEFAULT_SDK_VERSION);
 program.option("-u --username <USERNAME>", "Username for authentication.", DEFAULT_USERNAME);
 program.option("-P --password <USER_PASSWORD>", "Password for authentication.", DEFAULT_USER_PASSWORD);
 program.option("--org-id <ORGANIZATION_ID>", "Specify the organization.", DEFAULT_ORGANIZATION_ID);
 program.option("-v, --version <value>", "App version", DEFAULT_APP_VERSION);
-program.option("-i, --version-code <value>", "Version code (android only).", parseInt, DEFAULT_APP_VERSION_CODE);
+program.option("-i, --version-code <value>", "Version code (android only).", DEFAULT_APP_VERSION_CODE);
 program.option("-p, --platform <platform>", "Target build platform: Supported values are ios or android.", toLowerCase, DEFAULT_PLATFORM);
 program.option("-T, --target <value>", "Target to build for: dist-playstore, dist-appstore or dist-adhoc.", toLowerCase, DEFAULT_TARGET);
 program.option("-O, --output-dir <dir>", "Output directory.", DEFAULT_OUTPUT_DIR);
 program.option("-f, --force", "Force a full rebuild.");
 program.option("-b, --build-only", "Only brand the project; when specified, does not trigger a release.");
 program.option("--clean-only", "Clean the project by removing all branding information; when specified, does not trigger a build.");
-program.option("-l, --log-level <level>", "Minimum logging level. Supported options are trace, debug, info, warn, and error", "debug");
+program.option("-l, --log-level <level>", "Minimum logging level. Supported options are trace, debug, info, warn, and error", toLowerCase, "debug");
 program.parse(process.argv);
 
 /**
- * log level
+ * configuer logger
  */
-logger.setLevel(program.logLevel);
+log4js.configure({
+	appenders : [{
+		type : "console"
+	}]
+});
+log4js.setGlobalLogLevel(program.logLevel);
+
+//logger for Builder
+logger = log4js.getLogger("Builder");
 
 /**
  * client info
@@ -192,7 +200,7 @@ if (build) {
 		 * this is used just to keep the xml file formatted properly
 		 */
 		var tiapp = require("tiapp.xml").load(APP_TIAPP_XML);
-		tiapp.sdkVersion = program.sdkVersion;
+		tiapp.sdkVersion = program.sdk;
 		tiapp.version = program.version;
 
 		//android launcher acitivty name
@@ -339,7 +347,7 @@ if (build) {
  * exit if buildOnly is true
  */
 if (program.buildOnly) {
-	logger.info("Release ignored");
+	logger.info("Skipping release");
 	process.exit(0);
 } else {
 
@@ -469,7 +477,7 @@ if (program.buildOnly) {
 
 	//sdk version
 	appcParams.push("--sdk");
-	appcParams.push(program.sdkVersion);
+	appcParams.push(program.sdk);
 
 	//auth info
 	appcParams.push("--username");
@@ -478,6 +486,10 @@ if (program.buildOnly) {
 	appcParams.push(program.password);
 	appcParams.push("--org-id");
 	appcParams.push(program.orgId);
+
+	//log level
+	appcParams.push("--log-level");
+	appcParams.push(program.logLevel);
 
 	//run app using spwan
 	var appc = spawn("appc", appcParams);
@@ -493,6 +505,8 @@ if (program.buildOnly) {
 	appc.on("exit", function(code) {
 		if (code != 0) {
 			logger.error("appc exited with " + code);
+		} else {
+			logger.info("Finished release for " + program.platform + " " + client.client_name);
 		}
 		process.exit(code);
 	});
