8b0ad6128fcfe847c853cf010fd76334b3858e92
Manoj Kumar <mkumar@mscripts.com>
2015-12-08 14:58:23 +0530

    PHA-1510 - updated scheduled 2 drug error message

--- tools/assets/languages/en.js
+++ tools/assets/languages/en.js
@@ -88,7 +88,7 @@ module.exports = {
 		"msgAccountExists" : "It looks like you already have an account with %s",
 		"msgUserRecoverySuccess" : "We've just sent you an email with a password reset link. You can use it to create a new password.",
 		"msgMgrAccountCreation" : "Your manager account is created! Now please add your child.",
-		"msgPrescriptionSchedule2" : "Schedule 2 drugs cannot be refilled through this app. Please contact your doctor for more details.",
+		"msgPrescriptionSchedule2" : "Schedule 2 drugs cannot be refilled electronically. Contact your pharmacy or doctor for more information.",
 		"msgPrescriptionRefillLeftNone" : "This prescription has zero refills. We will need to contact your doctor to authorize this refill.",
 		"msgPrescriptionExpired" : "This prescription has expired. We will need to contact your doctor to authorize this refill.",
 		"msgPrescriptionHasMedReminder" : "You have already created a reminder that includes this prescription. Are you sure you want to select it for this reminder too?",
