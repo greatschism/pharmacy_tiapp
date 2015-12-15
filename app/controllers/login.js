8ae83f2fbc51b82390dcd567c5ee9c5cd51ab4b1
Kavitha Pai <kpai@mscripts.com>
2015-12-15 15:16:20 +0530

    Commented out the functionality mentioned in the above ticket as the flow is incomplete and Chris has to get back on this.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -152,7 +152,7 @@ function didAuthenticate() {
 	 * if so, take the user to log in screen.
 	 */
 	else if (args.is_adult_partial) {
-		if (args.origin === "registerChildInfo") {
+		//if (args.origin === "registerChildInfo") {
 			$.app.navigator.open({
 				titleid : "titleChildAdd",
 				ctrl : "childAdd",
@@ -163,13 +163,13 @@ function didAuthenticate() {
 				},
 				stack : false
 			});
-		} else {
+	/*} else {
 			$.app.navigator.open({
 				titleid : "titleAddFamily",
 				ctrl : "familyMemberAdd",
 				stack : true
 			});
-		}
+		}*/
 	} else if (mPatient.get("is_email_verified") !== "1" && moment.utc().diff(moment.utc(mPatient.get("created_at"), Alloy.CFG.apiCodes.ymd_date_time_format), "days", true) > 1) {
 		$.app.navigator.open({
 			ctrl : "emailVerify",
