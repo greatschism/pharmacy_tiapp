083bfc01b1fd95bbdbc20efa1707c929e5bcc0f4
Manoj Kumar <mkumar@mscripts.com>
2016-01-19 14:34:36 +0530

    refill reminders benefits android specifc fixes

--- app/controllers/remindersRefillBenefits.js
+++ app/controllers/remindersRefillBenefits.js
@@ -1,1 +1,9 @@
 var args = arguments[0] || {};
+
+function init(e) {
+	_.each(["refillView", "readyView", "pickupView"], function(val) {
+		$.uihelper.wrapViews($[val]);
+	});
+}
+
+exports.init = init;
