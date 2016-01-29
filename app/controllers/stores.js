49e7b36afbb6680ee8c4118e294f2d6e5f204c6b
Manoj Kumar <mkumar@mscripts.com>
2016-01-29 16:00:03 +0530

    PHA-2025 (revert)

--- app/controllers/stores.js
+++ app/controllers/stores.js
@@ -101,11 +101,11 @@ function didPostlayoutLoader(e) {
 }
 
 function didPauseApp(e) {
-	$.mapView && $.mapView.removeEventListener("regionchanged", didRegionchanged);
+	$.mapView && $.removeListener($.mapView, "regionchanged", didRegionchanged);
 }
 
 function didResumedApp(e) {
-	$.mapView && $.mapView.addEventListener("regionchanged", didRegionchanged);
+	$.mapView && $.addListener($.mapView, "regionchanged", didRegionchanged);
 }
 
 function setPatientSwitcher() {
