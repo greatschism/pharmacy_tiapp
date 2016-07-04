152148613c2a1f5d74dd68cf2ea4606e3facf86a
sowmyavn <snaaga@mscripts.com>
2016-07-04 11:40:19 +0530

    FVW-73
    FVW bugs addressed and branding done.

---
+++ tools/fairview/assets/data/language_en_1.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+	"data" : {
+
+		"strClientName" : "Fairview Pharmacy",
+
+		"titlePrescriptions" : "Refill from list",
+		"titleReminders" : "Reminders",
+		"titleRefill" : "Refill by scan",
+
+		"loginLblTitle" : "Sign in to the Fairview mobile pharmacy",
+		"familyMemberAddLbl" : "Great, let's add your family members!",
+		"titleTwitter" : "Twitter",
+		"titleFacebook" : "Facebook",
+		"titleYoutube"	:"Youtube",
+		"titlePharmacySpecials"	: "Pharmacy Specials"
+	}
+
+};
