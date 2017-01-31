49c7fadd543d2537551933b732ce94a10bf66b82
Sowmya VN <snaaga@mscripts.com>
2017-01-31 17:15:50 +0530

    UAT configuration and branding.

---
+++ tools/meijer/assets/data/language_en_1.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+	"data" : {
+
+		"strClientName" : "Meijer RX",
+
+		"titlePrescriptions" : "Refill from list",
+		"titleReminders" : "Reminders",
+		"titleRefill" : "Refill by scan",
+		
+		"loginLblTitle" : "Sign in to the Meijer Rx mobile pharmacy",
+		"familyMemberAddLbl" : "Great, let's add your family members!",
+		"titleTwitter" : "Twitter",
+		"titleFacebook" : "Facebook",
+		"titleYoutube"	:"Youtube"
+	}
+
+};
