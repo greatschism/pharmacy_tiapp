7c91f36dec17ee6e2a199d9701860e93fbd6885f
Harshal Hardiya <hhardiya@mscripts.com>
2017-05-10 17:52:50 +0530

    setting sample rx image on aspect ratio

--- app/controllers/rxSample.js
+++ app/controllers/rxSample.js
@@ -7,5 +7,15 @@ var parts = Alloy.Models.appload.get("sampl_rx_image").split('_'),
 						fractional = parts[1] || '';
 						
 $.img.image = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, fractional).read();
-$.img.width = utilities.percentageToValue("99%", app.device.width);
-$.img.height = utilities.percentageToValue("50%", app.device.height);
+setImageinAspectRatio();
+
+function setImageinAspectRatio() {
+	var desiredWidth = utilities.percentageToValue("99%", app.device.width);
+    var widthOfImage = $.img.image.width;  
+    var heightOfImage = $.img.image.height;
+
+	var aspectRatio =  heightOfImage / widthOfImage;
+    var desiredHeight = desiredWidth*aspectRatio;
+    $.img.width = desiredWidth;
+    $.img.height = desiredHeight;
+}
