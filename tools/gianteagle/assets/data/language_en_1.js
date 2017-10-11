06a0b3d2fb801ffb933ec21d96b74748629f5653
Rahul Kumar <rkumar@mscripts.com>
2017-10-11 17:17:18 +0530

    Giant Eagle Implementation

---
+++ tools/gianteagle/assets/data/language_en_1.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+	"data" : {
+
+		"strClientName" : "GiantEagle Rx",
+
+		"titlePrescriptions" : "Refill from list",
+		"titleReminders" : "Reminders",
+		"titleRefill" : "Refill by scan",
+		"childAddHintRxNo" : "Rx number (7 or 8 digits)",
+		"familyMemberAddPrescHintRxNo" : "Rx number (7 or 8 digits)",
+		"registerHintRxFormat" : "Rx number (7 or 8 digits)",
+
+		"titleTwitter" : "Twitter",
+		"titleFacebook" : "Facebook",
+		"titleYoutube"	:"Youtube",
+		"childAddValRxNoInvalid" : "Sorry, we don't recognize this prescription number. Please confirm that it's exactly what appears on the prescription label. It should be 7 or %d digits.",
+		"familyMemberAddPrescValRxNoInvalid" : "Sorry, we don't recognize this prescription number. Please confirm that it's exactly what appears on your prescription label. It should be 7 or %d digits.",
+        "refillTypeValRx" : "Your Rx number is incorrect, please enter the 7 or %d digit prescription number found on your prescription bottle.",
+		"registerValRxInvalid" : "Your Rx number is incorrect, please enter the 7 or %d digit prescription number found on your prescription bottle.",
+        "userRecoveryLbl":"Please enter the <tertiaryfont><tertiarycolor>email address</tertiarycolor></tertiaryfont> that you use to sign in.",
+		"userRecoveryEmailHint":"Email address",
+		"loginHintEmailOrUsername" : "Email address",
+        "userRecoveryValEmail":"Please enter your email address.",
+        "loginAttrForgotPassword":"<secondaryfont><secondarycolor>Forgot password?</secondaryfont></secondarycolor>",
+	    "userRecoveryAttrForgotUsername" : "<secondaryfont><secondarycolor> </secondaryfont></secondarycolor>",
+         "titleHelp" : "Help"
+	}
+
+};
