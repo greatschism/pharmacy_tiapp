c779a5429504452b30056a0378bb8c8192289802
Sowmya VN <snaaga@mscripts.com>
2017-10-27 16:17:48 +0530

    MCE-691

--- tools/assets/languages/en.js
+++ tools/assets/languages/en.js
@@ -349,7 +349,7 @@ module.exports = {
 		"loyaltySignupFeedbackNo" : "If you want to sign up please go to <secondaryfont><secondarycolor>www.meijer.com/mperks</secondarycolor></secondaryfont>",
 
 		"generateCodeBtn" : "Generate Pick Up QR code",
-		"expressCheckoutLblTitle" : "Enter your Date of Birth to enable your Express Pickup code. Use this code at the Express Pickup line at the pharmacy. The code will stay active for 24 hours. Re-enter your DOB to reactivate.",
+		"expressCheckoutLblTitle" : "Enter your date of birth to enable your Express Pickup code. The code will stay active for 24 hours. After 24 hours has elapsed, re-enter your date of birth to activate again.\n\nShow this code at the pharmacy counter to make picking up your prescriptions faster",
 		"generateValDob" : "Please enter your date of birth.",
 		"expressCheckoutMultipleStoreMsg" : "You have prescriptions ready in multiple locations. Express Pickup is not supported at multiple locations at this time. Please proceed to your pharmacy to pickup your prescriptions.",
 		"expressCheckoutDobMismatchMsg" : "This Date of Birth does not match our records, please reenter your information.",
