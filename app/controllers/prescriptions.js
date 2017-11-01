3066f7833fa93bd29984be533bcb20201baa5152
adlai_griffith <macsolve@gmail.com>
2017-11-01 15:12:43 -0500

    merge stragglers

--- app/controllers/prescriptions.js
+++ app/controllers/prescriptions.js
@@ -379,7 +379,11 @@ function prepareList() {
 					var promisedDate = moment(prescription.get("latest_refill_promised_date"), apiCodes.date_time_format),
 					    totalTime = promisedDate.diff(requestedDate, "seconds", true),
 					    timeSpent = currentDate.diff(requestedDate, "seconds", true);
-					subtitle = String.format($.strings.prescInProgressLblPromise, promisedDate.format(Alloy.CFG.day_of_week_time_format));
+						if (Alloy.CFG.show_promise_time_day_of_week === "showPromiseTimeDayOfWeek") {
+						subtitle = String.format($.strings.prescInProgressLblPromise, promisedDate.format(Alloy.CFG.day_of_week_time_format));
+						}	else {
+							subtitle = String.format($.strings.prescInProgressLblPromise, promisedDate.format(Alloy.CFG.date_time_format));
+						}
 					progress = Math.floor((timeSpent / totalTime) * 100);
 				} else {
 					subtitle = $.strings.strPrefixRx.concat(prescription.get("rx_number"));
@@ -493,10 +497,16 @@ function prepareList() {
 					});
 				}
 			}
+			var readyRxLabel;
+			if (Alloy.CFG.rx_number_for_ready_label === "rxNumberForReadyLabelEnabled") {
+				readyRxLabel = $.strings.strPrefixRx.concat(prescription.get("rx_number"));
+			}	else {
+				readyRxLabel = $.strings.prescReadyPickupLblReady;
+			}
 			prescription.set({
 				section : "readyPickup",
 				titleClasses : titleClasses,
-				subtitle : $.strings.strPrefixRx.concat(prescription.get("rx_number")),
+                subtitle : readyRxLabel,
 				canHide : false
 			});
 
