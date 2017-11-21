19edad079efe5fdc32a6e9f7c635ebad91669aa7
bijo274 <bbiswas@mscripts.com>
2017-11-21 13:58:24 +0530

    MCE-718

--- app/controllers/expressPickupBenefits.js
+++ app/controllers/expressPickupBenefits.js
@@ -6,19 +6,23 @@ function init()
 	
 }
 
-function didClickDone(){
-		if (currentPatient.get("mobile_number") && currentPatient.get("is_mobile_verified") === "1") {
-		$.app.navigator.open({
+function didClickDone() {
+	var currentPatient = Alloy.Collections.patients.findWhere({
+		selected : true
+	});
+  	
+	if (currentPatient.get("mobile_number") && currentPatient.get("is_mobile_verified") === "1") {
+  		$.app.navigator.open({
 			titleid : "titleHomePage",
 			ctrl : "home",
-			stack : false
-		});
+  			stack : false
+  		});
 	} else{
-		$.app.navigator.open({
+  		$.app.navigator.open({
 			titleid : "titleTextBenefits",
 			ctrl : "textBenefits",
-			stack : false
-		});
+  			stack : false
+  		});
 	};
-}
+  } 
 exports.init = init;
