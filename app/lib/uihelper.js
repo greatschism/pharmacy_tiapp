dcf31847fd01be219663754446bf243471233ce4
bijo274 <bbiswas@mscripts.com>
2017-12-06 13:14:00 +0530

    MCE-692

--- app/lib/uihelper.js
+++ app/lib/uihelper.js
@@ -957,21 +957,15 @@ var Helper = {
 				if (headerCheckoutTitle === "Checkout" || headerCheckoutTitle === $.strings.titleContinueExpressPickupHeader) {
 					var rightBtn = Ti.UI.createButton(rightItem);
 					if (OS_IOS) {
-						// var rightImg = Ti.UI.createImageView();
-						// rightImg.image = Helper.getImage("checkout_shopping_image").image;
-						// rightImg.height = 25;
-						// rightImg.top = '10pt';
-						// rightBtn.add(rightImg);
 						_.extend(rightBtn,$.createStyle({
-							classes:["i4","margin-right","light-blue-color","icon-checkout-cart"],
+							classes:["i4","margin-right","checkout-cart-color","icon-checkout-cart"],
 							width : 54,
 							height : 54
 							}));
 						headerViewHelp.add(rightBtn);
 					} else {
-						//rightBtn.image = Helper.getImage("checkout_shopping_image").image;
 						_.extend(rightBtn,$.createStyle({
-							classes:["i4","margin-right-small","light-blue-color","icon-checkout-cart"],
+							classes:["i4","margin-right-small","checkout-cart-color","icon-checkout-cart"],
 							width: 54,
 							height: 54
 						}));
