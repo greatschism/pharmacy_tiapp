02c67d4ce10636f2385eb3c3fa3f07ca327314b2
Rahul Kumar <rkumar@mscripts.com>
2017-12-14 13:05:17 +0530

    Vanderbilt Display Name Change

--- tools/vanderbilt/assets/data/language_en_1.js
+++ tools/vanderbilt/assets/data/language_en_1.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	"data" : {
 
-		"strClientName" : "Vanderbilt Health Pharmacy",
+		"strClientName" : "Vanderbilt",
 
 		"titlePrescriptions" : "Refill from list",
 		"titleReminders" : "Reminders",
@@ -24,6 +24,7 @@ module.exports = {
         "userRecoveryValEmail":"Please enter your email address.",
         "loginAttrForgotPassword":"<secondaryfont><secondarycolor>Forgot password?</secondaryfont></secondarycolor>",
 	    "userRecoveryAttrForgotUsername" : "<secondaryfont><secondarycolor> </secondaryfont></secondarycolor>",
+	    "loginLblTitle" : "Sign in to the %s Health Pharmacy",
          "titleHelp" : "Help"
 	}
 
