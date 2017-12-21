4ceb4360bc03e3c551abf5daa8a33c35035d0b81
adlai_griffith <macsolve@gmail.com>
2017-12-20 23:15:15 -0600

    sample for removing reminder toggle under condition of specialty store

--- app/controllers/prescriptionDetails.js
+++ app/controllers/prescriptionDetails.js
@@ -322,6 +322,13 @@ function didPostlayoutPromptSpecialty(e){
 		var source = e.source,
 	    children = source.getParent().children;
 	    source.removeEventListener("postlayout", didPostlayoutPromptSpecialty);
+
+		if(prescription.is_specialty_store) {
+			$.reminderRefillView.hide();
+			$.reminderRefillView.height = 0;
+
+		}
+
 	    if(prescription.is_specialty_store && prescription.store_phone){	    		    
 	    children[1].applyProperties({
 			left : children[1].left + children[0].rect.width,
