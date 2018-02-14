4508b3c2d98b0ccf015dc7232e01b4125b467d71
Rahul Kumar <rkumar@mscripts.com>
2018-02-14 15:34:32 +0530

    Reasors UAT Implementation

---
+++ tools/reasors/assets/data/language_en_1.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+	"data" : {
+
+		"strClientName" : "Reasors Rx",
+		"titlePrescriptions" : "Refill from list",
+		"titleReminders" : "Reminders",
+		"titleRefill" : "Refill by scan",
+        "userRecoveryLbl":"Please enter the <tertiaryfont><tertiarycolor>email address</tertiarycolor></tertiaryfont> that you use to sign in.",
+		"userRecoveryEmailHint":"Email address",
+		"loginHintEmailOrUsername" : "Email address",
+        "userRecoveryValEmail":"Please enter your email address.",
+        "loginAttrForgotPassword":"<secondaryfont><secondarycolor>Forgot password?</secondaryfont></secondarycolor>",
+	    "userRecoveryAttrForgotUsername" : "<secondaryfont><secondarycolor> </secondaryfont></secondarycolor>"
+	}
+
+};
