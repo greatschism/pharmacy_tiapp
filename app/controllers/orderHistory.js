f58778cd3f06fc14ee7d897643b44a9d900bd140
Sowmya VN <snaaga@mscripts.com>
2018-07-31 12:55:50 +0530

    ALBD-123

--- app/controllers/orderHistory.js
+++ app/controllers/orderHistory.js
@@ -14,23 +14,40 @@ var args = $.args,
 
 function init() {
 	analyticsCategory = require("moduleNames")[$.ctrlShortCode] + "-" + require("ctrlNames")[$.ctrlShortCode];
+	
+	/**
+	 * by default point to a
+	 * non partial account
+	 * only if patientSwitcherDisabled is false
+	 */
+	$.patientSwitcher.set({
+		revert : false,
+		title : $.strings.prescPatientSwitcher,
+		where : {
+			is_partial : false
+		}
+	});
 }
 
 function focus() {
 	if (!isWindowOpen) {
 		isWindowOpen = true;
 
-		$.http.request({
-			method : "order_history_get",
-			params : {
-				data : []
-			},
-			keepLoader : true,
-			errorDialogEnabled : true,
-			success : didGetOrderHistory,
-			failure : didFail
-		});
+		getOrderHistory();
 	}
+}
+
+function getOrderHistory() {
+	$.http.request({
+		method : "order_history_get",
+		params : {
+			data : []
+		},
+		keepLoader : true,
+		errorDialogEnabled : true,
+		success : didGetOrderHistory,
+		failure : didFail
+	});
 }
 
 function didGetOrderHistory(result, passthrough) {
@@ -39,18 +56,18 @@ function didGetOrderHistory(result, passthrough) {
 	var data = [],
 	    clientDateFormat = Alloy.CFG.date_format,
 	    serverDateFormat = Alloy.CFG.apiCodes.date_format;
-	if (result && result.data && result.data.OrderDetailsHistory.length) {
+	if (result && result.data && (result.data.OrderDetailsHistory != null)) {
 		orderDetailsData = result.data.OrderDetailsHistory;
 		_.each(result.data.OrderDetailsHistory, function(history) {
-			var totalPrice = history.totalRxPrice != null ? ( history.deliveryCost != null ? parseFloat(history.totalRxPrice) + parseFloat(history.deliveryCost) : parseFloat(history.totalRxPrice) ) : 0;
-			
+			var totalPrice = history.totalRxPrice != null ? (history.deliveryCost != null ? parseFloat(history.totalRxPrice) + parseFloat(history.deliveryCost) : parseFloat(history.totalRxPrice) ) : 0;
+
 			history = {
 				id : history.orderNumber,
-				title : "#"+ $.utilities.ucword(history.orderNumber),
-				subtitle : history.orderDate, //moment(history.orderDate, apiCodes.date_format).format(Alloy.CFG.date_format), //history.orderDate && moment(history.orderDate, serverDateFormat).format(clientDateFormat) || ""
-				detailTitle : totalPrice == 0 ? "$0" : "$"+totalPrice.toFixed(2),
+				title : "#" + history.orderNumber,
+				subtitle : moment(history.orderDate, apiCodes.date_format).format(Alloy.CFG.date_format), //history.orderDate && moment(history.orderDate, serverDateFormat).format(clientDateFormat) || ""
+				detailTitle : totalPrice == 0 ? "$0" : "$" + totalPrice.toFixed(2),
 				detailType : "positive"
-				// tertiaryTitle : 
+				// tertiaryTitle :
 			};
 			var row = Alloy.createController("itemTemplates/masterDetail", history);
 			data.push(row.getView());
@@ -58,6 +75,12 @@ function didGetOrderHistory(result, passthrough) {
 		});
 
 		$.tableView.setData(data);
+	} else {
+		$.uihelper.showDialog({
+			message : $.strings.orderHistoryMsgEmptyList,
+			buttonNames : [Alloy.Globals.strings.dialogBtnOK],
+			success : handleClose
+		});
 	}
 }
 
@@ -68,21 +91,71 @@ function didFail(result, passthrough) {
 	 * prevent any further actions
 	 */
 	$.app.navigator.hideLoader();
-	$.app.navigator.close();
+	handleClose();
 }
 
 function didClickTableView(e) {
-	
+	logger.debug("\n\n\n e 		", JSON.stringify(e, null, 4), "\n\n\n");
 	$.app.navigator.open({
 		titleid : "titleOrderHistory",
 		ctrl : "orderHistoryDetails",
 		ctrlArguments : {
-			orderDetailsData : orderDetailsData
+			orderDetailsData : orderDetailsData[e.index]
 		},
 		stack : true
 	});
+
+}
+
+function prepareData() {
+	var currentPatient = $.patientSwitcher.get();
+	if (currentPatient.get("is_partial")) {
+		$.partialDescLbl.text = $.strings.prescPartialLblDesc;
+		if (!$.partialView.visible) {
+			$.partialView.visible = true;
+		}
+	} else {
+		//hide if any
+		if ($.partialView.visible) {
+			$.partialView.visible = false;
+		}
+		getOrderHistory();
+	}
+}
+
+function didPostlayout(e) {
+	$.headerView.removeEventListener("postlayout", didPostlayout);
+	var top = $.headerView.rect.height,
+	    margin = $.tableView.bottom,
+	    bottom;
+	bottom = margin;
 	
+	$.tableView.applyProperties({
+		top : top,
+		bottom : bottom
+	});
+	$.partialView.applyProperties({
+		top : top,
+		bottom : bottom
+	});
+}
+
+function setParentView(view) {
+	$.patientSwitcher.setParentView(view);
+}
+
+function handleClose() {
+	$.app.navigator.open(Alloy.Collections.menuItems.findWhere({
+		landing_page : true
+	}).toJSON());
+}
+
+function terminate() {
+	//terminate patient switcher
+	$.patientSwitcher.terminate();
 }
 
 exports.init = init;
 exports.focus = focus;
+exports.terminate = terminate;
+exports.setParentView = setParentView;
--- app/controllers/orderHistoryDetails.js
+++ app/controllers/orderHistoryDetails.js
@@ -9,7 +9,9 @@ var args = $.args,
     subtitleClasses = ["margin-top-small", "left", "inactive-fg-color", "wrap-disabled"],
     detailClasses = ["margin-left-small", "custom-fg-color"],
     isWindowOpen,
-    orderDetailsData = args.orderDetailsData;
+    orderDetailsData = args.orderDetailsData,
+    postlayoutCount = 0,
+    rows = [];
 
 function init() {
 	analyticsCategory = require("moduleNames")[$.ctrlShortCode] + "-" + require("ctrlNames")[$.ctrlShortCode];
@@ -18,11 +20,128 @@ function init() {
 function focus() {
 	if (!isWindowOpen) {
 		isWindowOpen = true;
-		logger.debug("\n\n\n args.orderDetailsData		", JSON.stringify(args.orderDetailsData, null, 4),"\n\n\n");
+		logger.debug("\n\n\n args.orderDetailsData		", JSON.stringify(orderDetailsData, null, 4), "\n\n\n");
+
+		if (orderDetailsData) {
+			$.orderNumberLbl.text = "#" + orderDetailsData.orderNumber || null;
+			$.orderDateLbl.text = moment(orderDetailsData.orderDate, apiCodes.date_format).format(Alloy.CFG.date_format);
+
+			$.orderDeliveryTypeReplyLbl.text = orderDetailsData.deliveryType;
+			$.orderDeliveryCostReplyLbl.text = "$" + orderDetailsData.deliveryCost;
+			$.orderDeliveryAddressReplyLbl.text = orderDetailsData.deliveryAddress;
+			$.orderContactNumerReplyLbl.text = $.utilities.isPhoneNumber(orderDetailsData.mobileNumber) ? $.utilities.formatPhoneNumber(orderDetailsData.mobileNumber) : "";
+			$.orderPaymentMethodReplyLbl.text = orderDetailsData.cardType + " ending in " + orderDetailsData.lastFourDigit;
+
+			var totalPrice = orderDetailsData.totalRxPrice != null ? (orderDetailsData.deliveryCost != null ? parseFloat(orderDetailsData.totalRxPrice) + parseFloat(orderDetailsData.deliveryCost) : parseFloat(orderDetailsData.totalRxPrice) ) : 0;
+
+			$.orderTotalCostReplyLbl.text = totalPrice == 0 ? "$0" : "$" + totalPrice.toFixed(2);
+			$.prescAsyncView.hide();
+
+			var data = [];
+
+			if (orderDetailsData.prescriptions.length) {
+				_.each(orderDetailsData.prescriptions, function(prescription) {
+
+					prescription = {
+						id : prescription.rxNumber,
+						title : $.utilities.ucword(prescription.rxName),
+						subtitle : "#" + prescription.rxNumber,
+						detailTitle : prescription.copay == 0 ? "$0" : "$" + parseFloat(prescription.copay).toFixed(2),
+						detailType : "positive"
+						// tertiaryTitle :
+					};
+					var row = Alloy.createController("itemTemplates/masterDetail", prescription);
+					data.push(row.getView());
+					rows.push(row);
+				});
+
+				$.tableView.setData(data);
+			}
+			setTimeout(didUpdateUI, 1000);
+		}
+
 	}
 }
 
+function didUpdateUI() {
+	/**
+	 * PHA-1086 - keep it expanded
+	 * incase to revert:
+	 * 1. Update the toggle
+	 * (show more / less) title in xml
+	 * 2. remove $.prescExp.expand();
+	 * 3. keep only $.loader.hide();
+	 * 4. move this setTimeout(didUpdateUI, 1000);
+	 * to init
+	 */
+	$.prescExp.expand();
+	if (Ti.App.accessibilityEnabled) {
+		togglePrescription();
+	};
+	$.loader.hide();
+}
+
 function togglePrescription(e) {
+	var title,
+	    result;
+	if ($.prescExp.isExpanded()) {
+		title = "prescDetExpand";
+		result = $.prescExp.collapse();
+	} else {
+		title = "prescDetCollapse";
+		result = $.prescExp.expand();
+	}
+	if (result) {
+		// $.toggleLbl.text = $.strings[title];
+	}
+	// $.loader.hide();
+
+}
+
+function didClickShowPrescriptions() {
+	$.rxContainerView.show();
+}
+
+function didClickHide(e) {
+	$.rxContainerView.hide();
+}
+
+function didPostlayoutPrompt(e) {
+	logger.debug("\n\n\n in didPostlayoutPrompt\n\n\n");
+
+	var source = e.source,
+	    children = source.getParent().children;
+	// source.removeEventListener("postlayout", didPostlayoutPrompt);
+	children[1].applyProperties({
+		left : children[1].left + children[0].rect.width,
+		visible : true
+	});
+	postlayoutCount++;
+	if (postlayoutCount === 0) {
+		$.prescExp.setStopListening(true);
+		// $.loader.hide();
+		logger.debug("\n\n\n stop listening\n\n\n");
+	}
+}
+
+function didPostlayout(e) {
+	/*
+	$.rxContainerView.removeEventListener("postlayout", didPostlayout);
+		var top = $.tableView.top,
+			margin = $.tableView.bottom,
+			bottom;
+		bottom = margin;
+		bottom = bottom + $.hideBtn.height * 2;
+	
+		$.tableView.applyProperties({
+			top : top,
+			bottom : bottom
+		});
+		
+		$.hideBtn.applyProperties({
+			top : bottom
+		});*/
+	
 }
 
 exports.init = init;
