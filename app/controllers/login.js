e72e3d6ae903417f0baad60fe56c5ebe31340c80
Bijoy Krishna Biswas <bbiswas@mscripts.com>
2018-08-10 13:27:50 +0530

    SUS-1612

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -257,9 +257,11 @@ function didClickLogin(e) {
 		});
 		return;
 	}
-	if ($.utilities.isPhoneNumber(username)) {
-		//yet to handle
-	} else {
+/*
+	 if ($.utilities.isPhoneNumber(username)) {
+		// yet to handle
+	 } */
+	else {
 		authenticator.setAutoLoginEnabled($.autoLoginSwt.getValue());
 
 
