649d90bf100e2be4b62a0d5381111dcbc569c2db
Adlai <adlaigriffith@Macsolvings-MacBook-Pro.local>
2018-09-27 22:02:43 -0500

    in ios, the continueactivity event isn't fired when the app is launched 'cold' via deeplink.  The presumdly only works when bound in Alloy.js

--- app/lib/ios/deeplinkHandler.js
+++ app/lib/ios/deeplinkHandler.js
@@ -7,17 +7,23 @@ function init() {
 	 * 	url passed via associated domain captured
 	 *  here and assigned to Alloy.Globals.url
 	 */
-	Ti.App.iOS.addEventListener('continueactivity', function(e) {
-		if (e.activityType === "NSUserActivityTypeBrowsingWeb") {
-			Ti.API.info(JSON.stringify(e.webpageURL));
-			/**
-			 * 	Handle the URL in case it opened the app
-			 */ 
-			if ( typeof e.webpageURL === 'string') {
-				Alloy.Globals.url = e.webpageURL;
-			}
-		}
-	});
+	// Ti.API.info('deeplink handler init');
+	// Ti.App.iOS.addEventListener('continueactivity', function(e) {
+	// 	Ti.API.info('continueactivity is FIRED');
+	// 	if (e.activityType === "NSUserActivityTypeBrowsingWeb") {
+	// 		Ti.API.info('(e.activityType === "NSUserActivityTypeBrowsingWeb") ' +JSON.stringify(e.webpageURL));
+	// 		/**
+	// 		 * 	Handle the URL in case it opened the app
+	// 		 */ 
+	// 		if ( typeof e.webpageURL === 'string') {
+	// 			Alloy.Globals.url = e.webpageURL;
+	// 		}
+	// 	}
+	// });
+
+	//The above code has been commented out.  The continueactivity event needs to be bound in alloy.js in order to fire when the app is opened from being completely closed.
+	//This logic has been moved back to Alloy.js
+
 }
 
 exports.init = init; 
